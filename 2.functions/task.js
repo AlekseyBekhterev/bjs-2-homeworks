0ca7341fa1edf30c5b75fb642c479de3fcb3be9a
AlekseyBekhterev <beh45@mail.ru>
2022-01-30 22:37:35 +0300

    сделал 1 и частично вторую задачу

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,29 +1,65 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > max) {  
+      max = arr[i]; 
+    } 
+    if (arr[i] < min) {
+      min = arr[i];
+    }   
+    sum = sum + arr[i]
 }
+avg = (sum/arr.length).toFixed(2);
+
+  return { min: min, max: max, avg: Number(avg) };
+}
+
+let callFunc = getArrayParams (arr)
+
 
 // Задание 2
+let arrOfArr=[[1, 2, 3], [4, 5, 6]];
+
 function worker(arr) {
-  let sum;
+  let sum = 0;
+  let sum1 =0
+  let sum2 = 0;
+    for(let i = 0; i < arrOfArr.length; i++){
+        for (let k = 0; k < arrOfArr[i].length; k++) {   
+        console.log(sum1 = sum1 + arrOfArr[0][k], 'первый элемент')
+        console.log(sum2 = sum2 + arrOfArr[1][k], 'второй элемент')  
+        
+        } 
+        
+        break
 
-  // Ваш код
-
-  return sum;
+    }
+    if(sum1>sum2) {
+        sum = sum1
+    } else {
+        sum = sum2
+    }
+    
+return sum
 }
+let getWorker = worker();
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
 
-  // Ваш кода
-  // for ...
+  max = console.log(getWorker);
   
   return max;
 }
+let getMakeWork = makeWork();
+console.log(getMakeWork);
+
+
+
 
 // Задание 3
 function worker2(arr) {
